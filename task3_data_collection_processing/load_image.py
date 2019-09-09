83df8b576a5cefc8f842f42d6d5498e1da23f4bd
ManuelMourato25 <manuelmourato25@gmail.com>
2019-09-09 12:49:32 +0000

    Optimized script runtime + added log messages

--- task3_data_collection_processing/load_image.py
+++ task3_data_collection_processing/load_image.py
@@ -6,7 +6,6 @@ from PIL import Image
 import os
 import glob
 import ntpath
-from osgeo import gdal
 import rasterio as rio
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -26,6 +25,7 @@ class Bands_to_RGB:
             Loads all bands paths from one Landsat8 image to a list
             @return: list of paths
             '''
+            logging.info("Loading Image from "+ directory)
             image_dict = {}
             image_list = []
             for filename_path in glob.glob(directory + '*band*.tif'): 
@@ -56,24 +56,32 @@ class Bands_to_RGB:
             Generates stacked image from all bands
             void
             '''
-
-            stack_data, metadata = es.stack(paths_list[:7] , HOME_DIR + stacked_file_output)
+            logging.info("Generating stacked image from bands")
+            
+            if os.path.isfile( HOME_DIR + stacked_file_output):
+                
+                logging.info("Stacked image already exists. Skipping")
+            else:
+                stack_data, metadata = es.stack(paths_list[:7] , HOME_DIR + stacked_file_output)
          
         def create_rgb_plots(self, HOME_DIR, plot_to_generate, stacked_file_output):
             with rio.open(HOME_DIR + stacked_file_output) as src:
                 write_operation = src.read()                
                 
                 if plot_to_generate == '1' or plot_to_generate == 'all':
+                    logging.info("Plotting Natural Color image")
                     plot_rgb(write_operation,
                             rgb=[3, 2, 1],
                 title="Natural Color", dest_file=HOME_DIR + "rgb_image_natural_color")
 
                 if plot_to_generate == '2' or plot_to_generate == 'all':
+                    logging.info("Plotting Color Infrared (vegetation) image")
                     plot_rgb(write_operation,
                             rgb=[4, 3, 2],
                 title="Color Infrared (vegetation)", dest_file=HOME_DIR + "rgb_image_infrared")
         
                 if plot_to_generate == '3' or plot_to_generate == 'all':
+                    logging.info("Plotting Vegetation Analysis image")
                     plot_rgb(write_operation,
                             rgb=[5, 4, 3],
                 title="Vegetation Analysis", dest_file=HOME_DIR + "_" + "rgb_image_vegitation")
@@ -92,7 +100,11 @@ class Bands_to_RGB:
                         (nir + red) == 0.,
                         0,
                         (nir - red) / (nir + red))
+                    
+                    logging.info("Calculating Average NDVI for this image")
+                    logging.info(np.average(ndvi))
 
+                    logging.info("Plotting NDVI image")
                     ndviImage = rio.open(HOME_DIR + 'ndviImage.tiff', 'w', driver='Gtiff',
                           width=src.width,
                           height=src.height,
@@ -106,24 +118,28 @@ class Bands_to_RGB:
                     plt.close
 
                 if plot_to_generate == '5' or plot_to_generate == 'all':
+                        logging.info("Plotting False Color Urban image")
                         plot_rgb(write_operation,
                             rgb=[6, 5, 3],
                             title="False Color Urban", dest_file=HOME_DIR + "rgb_image_urban")
         
                 if plot_to_generate == '6' or plot_to_generate == 'all':
+                        logging.info("Plotting Agriculture image")
                         plot_rgb(write_operation,
                             rgb=[5, 4, 1],
                             title="Agriculture", dest_file=HOME_DIR + "rgb_image_agriculture")
         
                 if plot_to_generate == '7' or plot_to_generate == 'all':
+                        logging.info("Plotting Atmospheric Penetration image")
                         plot_rgb(write_operation,
                             rgb=[6, 5, 4],
                             title="Atmospheric Penetration", dest_file=HOME_DIR + "rgb_image_atmospheric_penetration")
                 
                 if plot_to_generate == '8' or plot_to_generate == 'all':
+                        logging.info("Plotting Healthy Vegetation image")
                         plot_rgb(write_operation,
                             rgb=[4, 5, 1],
-                            title="Healthy Vegitation", dest_file=HOME_DIR + "rgb_image_healthy_vegetation")
+                            title="Healthy Vegetation", dest_file=HOME_DIR + "rgb_image_healthy_vegetation")
  
                 if plot_to_generate == '9' or plot_to_generate == 'all':
                     '''
@@ -139,6 +155,11 @@ class Bands_to_RGB:
                         (swir + green) == 0.,
                         0,
                         (green - swir) / (green + swir))
+                    
+                    logging.info("Calculating Average NDWI for this image")
+                    logging.info(np.average(ndwi))
+                    
+                    logging.info("Plotting NDWI image")
                     ndwiImage = rio.open(HOME_DIR + 'ndwiImage.tiff', 'w', driver='Gtiff',
                           width=src.width,
                           height=src.height,
@@ -152,11 +173,13 @@ class Bands_to_RGB:
                     plt.close
       
                 if plot_to_generate == '10' or plot_to_generate == 'all':
+                        logging.info("Plotting Atmospheric Removal image")
                         plot_rgb(write_operation,
                             rgb=[6, 4, 2],
                             title="Atmospheric Removal", dest_file=HOME_DIR + "rgb_image_atmospheric_removal")
                     
                 if plot_to_generate == '11' or plot_to_generate == 'all':
+                        logging.info("Plotting Short wave infrared image")
                         plot_rgb(write_operation,
                             rgb=[6, 4, 3],
                             title="Short wave infrared", dest_file=HOME_DIR + "rgb_image_short_wave_infrared")
@@ -229,7 +252,7 @@ parser.add_argument('ptg', metavar='ptg', type=str,
 
 args = parser.parse_args()
 
-
+logging.getLogger().setLevel(logging.INFO)
 bands_to_rgb = Bands_to_RGB() 
 paths_list = bands_to_rgb.load_bands_list(args.homeDir)
 bands_to_rgb.create_stacked_image(args.homeDir, args.stckof, paths_list)
