591c32ca6b67c4aa95d1f23fb590716d2be09223
Vishal R <vishal27.rs@outlook.com>
2019-09-24 22:31:15 +0530

    Add methods to download bands

--- task4_ image_segmentation/subtask4_1_anomaly_detection/gcloud_landsat_data_utils.py
+++ task4_ image_segmentation/subtask4_1_anomaly_detection/gcloud_landsat_data_utils.py
@@ -9,6 +9,11 @@ import ogr
 import shapely.wkt
 import shapely.geometry
 import os
+import re
+import logging
+from pathlib import Path
+
+get_dir = lambda n : n if len(n) == 3 else '0'*(3 - len(n)%3)+n
 
 def list_blobs_with_prefix(bucket_name: str, prefix: str, delimiter=None):
     """
@@ -24,10 +29,10 @@ def get_all_dates(path: str, row: str):    # Pass path and row as strings
     """
     Get all unique dates of images in a Path/Row
     """
-    get_dir = lambda n : n if len(n) == 3 else '0'*(3 - len(n)%3)+n
+    global get_dir
+    prefix = 'LC08/01/{}/{}/'.format(path, row)
     path = get_dir(path)
     row = get_dir(row)
-    prefix = 'LC08/01/{}/{}/'.format(path, row)
     blobs = list_blobs_with_prefix('gcp-public-data-landsat', prefix)
     dates = list(set(i.name.split('/')[-1].split('_')[3] for i in blobs))
     dates = [dt.datetime.strptime(i, '%Y%m%d') for i in dates]
@@ -61,6 +66,59 @@ def get_pathrow_from_latlon(lat, lon):
     row = feature['ROW']
     return path, row
 
+def get_blob_names(prefix, path, row, date, bands):
+    global get_dir
+    patterns = [re.compile("{}.*_{}{}_{}_.*_B{}\D.*".format(prefix, path, row, date, band)) for band in bands]
+    path = get_dir(path)
+    row = get_dir(row)
+    blobs = list_blobs_with_prefix('gcp-public-data-landsat', prefix)
+    blob_names = []
+    for blob in blobs:
+        for pattern in patterns:
+            if pattern.match(blob.name):
+                blob_names.extend([blob.name])
+    return blob_names
+
+def get_filename_from_blobname(blob_name):
+    return blob_name.split('/')[-1]
+
+def create_download_directory(path, row, date):
+    pathrow_dir = Path("{}{}".format(path, row))
+    if not os.path.isdir(pathrow_dir):
+        os.makedirs(pathrow_dir)
+        os.makedirs(pathrow_dir / date)
+        logging.info('Download directories created.')
+    elif not os.path.isdir(pathrow_dir / date):
+        os.makedirs(pathrow_dir / date)
+        logging.info('Download directories created.')
+    else:
+        logging.info("Download directories aready present. Skipping")
+    return pathrow_dir / date
+
+def get_bands(path, row, date: str, bands): # dates in "yyyymmdd" format and bands as an iterable
+    global get_dir
+    logging.getLogger().setLevel(logging.INFO)
+    path = get_dir(path)
+    row = get_dir(row)
+    prefix = 'LC08/01/{}/{}/'.format(path, row)
+    logging.info('Getting Blob names')
+    blob_names = get_blob_names(prefix, path, row, date, bands)
+    if len(blob_names) != 0:
+        logging.info("Found {} blobs".format(len(blob_names)))
+        logging.info('Creating download directories.')
+        download_dir = create_download_directory(path, row, date)
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket('gcp-public-data-landsat')
+        logging.info("Beginning Downloads")
+        for i in blob_names:
+            blob = bucket.blob(i)
+            filename = get_filename_from_blobname(i)
+            blob.download_to_filename(download_dir / filename)
+            logging.info('\tPathRow: {}{} \tDate: {} \tDownloaded to: {}'.format(path, row, date, download_dir))
+        print("Done")
+    else:
+        print("No files to download")
 
 # path, row =  get_pathrow_from_latlon(4.1761213,45.0235419)
 # get_all_dates(path, row)
+#get_bands('164', '57', '20141005', [1, 2, 3])
